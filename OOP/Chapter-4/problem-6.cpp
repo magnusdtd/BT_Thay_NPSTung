79a4bbddc88fd172f38897f4f812d406755bb059
Đàm Tiến Đạt <tdat0306@gmail.com>
2025-03-19 14:24:43 +0000

    Merge branch cungh to main

--- OOP/Chapter-4/problem-6.cpp
+++ OOP/Chapter-4/problem-6.cpp
@@ -1,23 +1,95 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <fstream>
 using namespace std;
 
-int main() {
-    fstream in("input.txt", ios::in), out("output.txt", ios::out);
+class Character{
+private:
+    string name;
+public:
+    Character();
+    Character(string name);
+    void setName(string name);
+    string getName();
+};
 
-    if (!in.is_open() || !out.is_open()) {
-        cout << "Error opening file!\n";
-        return 1;
+Character::Character(){
+    name = "";
+}
+
+Character::Character(string name){
+    this->name = name;
+}
+
+void Character::setName(string name){
+    this->name = name;
+}
+
+string Character::getName(){
+    return name;
+}
+
+class List{
+private:
+    vector<Character> characters;
+    int size;
+public:
+    List();
+    string getMaxLengthName();
+    void add(Character character);
+    void input(string filename);
+    void output(string filename);
+};
+
+List::List() {
+    size = 0;
+}
+
+void List::add(Character character){
+    characters.push_back(character);
+    size++;
+}
+
+void List::input(string filename){
+    ifstream in;
+    in.open(filename);
+    if(!in.is_open()){
+        cerr << "Error opening file" << "\n";
+        exit(1);
     }
-
-    string name, longestName;
-    while (getline(in, name))
-        if (name.size() > longestName.size())
-            longestName = name;
+    string name;
+    while(getline(in, name)){
+        Character character(name);
+        add(character);
+    }
     in.close();
+}
 
-    if (!longestName.empty()) 
-        out << longestName << " " << longestName.size() << "\n";
+
+string List::getMaxLengthName(){
+    string maxLengthName = "";
+    for(int i = 0; i < size; i++)
+        if(characters[i].getName().length() > maxLengthName.length())
+            maxLengthName = characters[i].getName();
+    return maxLengthName;
+}
+
+void List::output(string filename){
+    ofstream out;
+    out.open(filename);
+    if(!out.is_open()){
+        cerr << "Error opening file" << "\n";
+        exit(1);
+    }
+    string maxLengthName = getMaxLengthName();
+    out << maxLengthName << " " << maxLengthName.length() << "\n";
     out.close();
+}
 
+int main(){
+    List l;
+    l.input("input.txt");
+    l.output("output.txt");
+    return 0;
 }
